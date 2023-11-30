5b8f4fb8c826272016e958018da25221a761685d
Shahid M <120563988+mshahid153@users.noreply.github.com>
2023-11-30 22:54:23 +0530

    Initial Commit

---
+++ src/HistoryItem/index.js
@@ -0,0 +1,37 @@
+import './index.css'
+
+const HistoryItem = props => {
+  const {historyItemDetails, onDeleteFn} = props
+  const {id, timeAccessed, logoUrl, title, domainUrl} = historyItemDetails
+
+  const onDeleteItem = () => {
+    onDeleteFn(id)
+  }
+
+  return (
+    <li className="bh-each-list-container">
+      <p className="bh-time">{timeAccessed}</p>
+      <div className="bh-history-item-container">
+        <div className="bh-history-item">
+          <img className="bh-domain-logo" src={logoUrl} alt="domain logo" />
+          <div className="bh-title-container">
+            <p className="bh-title">{title}</p>
+            <p className="bh-domain-url">{domainUrl}</p>
+          </div>
+        </div>
+        <button
+          className="bh-delete-button"
+          type="button"
+          onClick={onDeleteItem}
+        >
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+            alt="delete"
+          />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default HistoryItem
